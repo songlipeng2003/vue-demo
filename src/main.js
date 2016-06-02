fa7a7ddd65e5e05f8a31da1770dab7c5201333a9
Thinking Song <123wuming_123@163.com>
2016-06-02 21:53:09 +0800

    add vue  for tab

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+import {Style} from 'vux-components'
 
 /* eslint-disable no-new */
 new Vue({
   el: 'body',
-  components: { App }
+  components: { App, Style }
 })
+
+Vue.use(VueRouter)
+Vue.use(VueResource)
